8625eae4b059673495c4fec31c821d0f3a0022b8
OteIan <oteian9@gmail.com>
2024-08-18 16:44:53 +0300

    hooks-state-mini-project done

--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,16 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 
-function NewTaskForm() {
+function NewTaskForm({ categories, onTaskFormSubmit }) {
+  const [text, setText] = useState("");
+  const [category, setCategory] = useState("All");
+
+  const filteredCategories = categories.filter((category) => category !== "All");
+
+  function handleTextChange(e) {
+    setText(e.target.value);
+  }
+
+  function handleCategoryChange(e) {
+    setCategory(e.target.value)
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    onTaskFormSubmit({
+      text: text,
+      category: category
+    })
+
+    setText("");
+    setCategory("All");
+  }
+
   return (
-    <form className="new-task-form">
+    <form className="new-task-form" onSubmit={handleSubmit}>
       <label>
         Details
-        <input type="text" name="text" />
+        <input
+          type="text"
+          name="text"
+          value={text}
+          onChange={handleTextChange}
+        />
       </label>
       <label>
         Category
-        <select name="category">
+        <select name="category" onChange={handleCategoryChange}>
           {/* render <option> elements for each category here */}
+          {filteredCategories.map((category, index) => (
+            <option key={index} value={category}>{category}</option>
+          ))}
         </select>
       </label>
       <input type="submit" value="Add task" />
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Task from "./Task";
 
-function TaskList() {
+function TaskList({ tasks }) {
+  const [taskData, setTaskData] = useState(tasks);
+
+  useEffect(() => setTaskData(tasks), [tasks])
+
+  function removeTask(id) {
+    setTaskData(taskData.filter((_task, index) => index !== id));
+  }
+
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      {taskData.map((task, index) => (
+        <Task
+          key={index}
+          id={index}
+          category={task.category}
+          text={task.text}
+          removeTask={removeTask}
+        />
+      ))}
     </div>
   );
 }
